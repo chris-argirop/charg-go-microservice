e6dc40e5dce2241f61cfc0de32316662b28111c2
Chris Arg <argirop.chris@gmail.com>
2022-10-27 22:14:51 +0300

    hidden output from db connection attempts

--- db-waitingroom.sh
+++ db-waitingroom.sh
@@ -5,9 +5,9 @@ COMMAND=$@
 
 echo 'Waiting for database to be available...'
 maxTries=10
-while [ "$maxTries" -gt 0 ] && ! mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DB" -e 'SHOW TABLES'; do
+while [ "$maxTries" -gt 0 ] && ! mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DB" -e 'SHOW TABLES' > /dev/null 2>&1; do
     maxTries=$(($maxTries - 1))
-    echo 'Retrying...'
+    echo 'Retrying to connect to MySQL Database...'
     sleep 3
 done
 echo
